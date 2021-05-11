cb2302c0a88e1ac47a48010863f771a400798982
Karmel <kszlagk@gmail.com>
2021-05-11 18:55:58 +0200

    Button's Milestone!
    Finally creating and adding marks works as intended, welp, it isnt goint right ot the cursor position and that stuff but it creates and manages Markers as i wanted it to do.

--- Windows/MainWindow.xaml.cs
+++ Windows/MainWindow.xaml.cs
@@ -32,10 +32,27 @@ namespace csgo_minimap_radar_helper
             { "hostage", new BitmapImage(new Uri("pack://application:,,,/Resources/hostage.png")) }
         };
 
+        public Dictionary<string, Image> existingRadarMarkers = new()
+        {
+            { "", new Image() }
+        };
+
         public MainWindow()
         {
             InitializeComponent();
             defaultNoRadarImage = RadarImage.Source;
+
+            radarMarkerButtons.Add(ct_spawn_button);
+            radarMarkerButtons.Add(t_spawn_button);
+
+            radarMarkerButtons.Add(bomb_a_button);
+            radarMarkerButtons.Add(bomb_b_button);
+
+            radarMarkerButtons.Add(hostage1_button);
+            radarMarkerButtons.Add(hostage2_button);
+            radarMarkerButtons.Add(hostage3_button);
+            radarMarkerButtons.Add(hostage4_button);
+            radarMarkerButtons.Add(hostage5_button);
         }
 
         private void OnLoadRadarClick(object sender, RoutedEventArgs e)
@@ -57,22 +74,49 @@ namespace csgo_minimap_radar_helper
         private void OnRadarImageClick(object sender, MouseButtonEventArgs e)
         {
             RadarImage.Cursor = Cursors.Arrow;
+            if (lastSelectedButton == null) return;
+            if (string.IsNullOrEmpty(lastSelectedButton.Name)) return;
+            lastSelectedButtonName = "Update";
+            UpdateEverySelectionButtonState();
 
-            switch (selectedSelectionButton)
+            string markerName = GetMarkerNameByItsButtonName(lastSelectedButton.Name);
+            Image imgObject = new();
+
+            string defResImg = markerName;
+
+            switch (defResImg)
             {
-
+                case "hostage1":
+                case "hostage2":
+                case "hostage3":
+                case "hostage4":
+                case "hostage5":
+                    defResImg = "hostage";
+                break;
             }
-            Image test = new()
+
+            imgObject.Source = defaultResourceImages[defResImg];
+            imgObject.Width = 50;
+            imgObject.Height = 50;
+
+            Canvas.SetLeft(imgObject, (RadarCanvas.Width / 2) - imgObject.Width / 2);
+            Canvas.SetTop(imgObject, (RadarCanvas.Height / 2) - imgObject.Height / 2);
+
+            radarMarkers[markerName] = new RadarMarker(
+                true,
+                imgObject.Width,
+                imgObject.Height,
+                imgObject
+            );
+            // [THIS GUD CODE BUT DISABLED FOR DEBUG]
+            RadarCanvas.Children.Add(imgObject);
+           
+            Label myText = new()
             {
-                Source = defaultResourceImages["ct_spawn"],
-                Width = 32,
-                Height = 32
+                Content = $"X: {RadarCanvas.Width} Y: {RadarCanvas.Height}"
             };
-
-            Canvas.SetLeft(test, RadarCanvas.Width / 2);
-            Canvas.SetTop(test, RadarCanvas.Height / 2);
-
-            RadarCanvas.Children.Add(test);
+            RadarCanvas.Children.Add(myText);
+            
         }
 
         private void RadarImage_ImageFailed(object sender, ExceptionRoutedEventArgs e)
@@ -80,30 +124,84 @@ namespace csgo_minimap_radar_helper
             RadarImage.Source = defaultNoRadarImage;
         }
 
-        private void UpdateEverySelectionButtonState(string state)
-        { 
-            
+        public static List<Button> radarMarkerButtons = new();
+
+        private void UpdateEverySelectionButtonState()
+        {
+            if (lastSelectedButton != null)
+            {
+                lastSelectedButton.Content = lastSelectedButtonName;
+            }
+
+            foreach (Button btn in radarMarkerButtons)
+            {
+                if ((string)btn.Content != "Add")
+                {
+                    btn.Content = "Update";
+                }
+                else
+                {
+                    btn.Content = "Add";
+                }
+            }
+
         }
 
-        private Button GetMainButtonBasedOfHisRestartButton(string restartButtonName)
+        private Button GetMainButtonByItsRestartButton(string restartButtonName)
         {
-            switch (restartButtonName)
-            { 
-                
+            return radarMarkerButtons.Find(item => item.Name == restartButtonName.Substring(0, restartButtonName.Length - 2));
+        }
+
+        private string GetMarkerNameByItsButtonName(string buttonName)
+        {
+            switch (buttonName)
+            {
+                case "ct_spawn_button":
+                    return "ct_spawn";
+                case "t_spawn_button":
+                    return "t_spawn";
+
+                case "bomb_a_button":
+                    return "bomb_a";
+                case "bomb_b_button":
+                    return "bomb_b";
+
+                case "hostage1_button":
+                    return "hostage1";
+                case "hostage2_button":
+                    return "hostage2";
+                case "hostage3_button":
+                    return "hostage3";
+                case "hostage4_button":
+                    return "hostage4";
+                case "hostage5_button":
+                    return "hostage5";
             }
             return null;
         }
 
-        static string selectedSelectionButton = "";
+        static string lastSelectedButtonName = string.Empty;
+        static Button lastSelectedButton;
 
-        private void OnStartSelectingRadar_Click(object sender, ExceptionRoutedEventArgs e)
+        private void OnStartSelectingRadar_Click(object sender, RoutedEventArgs e)
         {
-            RadarImage.Cursor = Cursors.Cross;
+            if ((string)((Button)sender).Content == "Cancel")
+            {
+                RadarImage.Cursor = Cursors.Arrow;
+                ((Button)sender).Content = lastSelectedButtonName;
 
-            UpdateEverySelectionButtonState("Select");
-            ((Button)sender).Content = "Cancel";
+                lastSelectedButtonName = null;
+            }
+            else
+            {
+                RadarImage.Cursor = Cursors.Cross;
 
-            selectedSelectionButton = ((Button)sender).Name;
+                UpdateEverySelectionButtonState();
+                lastSelectedButtonName = (string)((Button)sender).Content;
+                ((Button)sender).Content = "Cancel";
+
+                lastSelectedButton = (Button)sender;
+            }
         }
 
         private void OnCancelSelectingRadar_Click(object sender, ExceptionRoutedEventArgs e)
@@ -112,26 +210,34 @@ namespace csgo_minimap_radar_helper
 
             ((Button)sender).Content = "Select";
 
-            selectedSelectionButton = "";
+            lastSelectedButton = null;
         }
 
-        private void OnRestartRadarMarker_Click(object sender, ExecutedRoutedEventArgs e)
+        private void OnRestartRadarMarker_Click(object sender, RoutedEventArgs e)
         {
-            
+            Button desiredMainButton = GetMainButtonByItsRestartButton(((Button)sender).Name);
+            string desiredRMName = GetMarkerNameByItsButtonName(desiredMainButton.Name);
+            RadarCanvas.Children.Remove(radarMarkers[desiredRMName].OnScreenImage);
+            radarMarkers[desiredRMName] = new();
+            desiredMainButton.Content = "Add";
+            if (lastSelectedButton == desiredMainButton)
+            {
+                lastSelectedButtonName = "Add";
+            }
         }
 
         public Dictionary<string, RadarMarker> radarMarkers = new()
         {
             { "ct_spawn", new RadarMarker() },
-            { "t_spawn", new RadarMarker()  },
-            { "bomb_a", new RadarMarker()  },
-            { "bomb_b", new RadarMarker()  },
+            { "t_spawn", new RadarMarker() },
+            { "bomb_a", new RadarMarker() },
+            { "bomb_b", new RadarMarker() },
 
-            { "hostage1", new RadarMarker()  },
-            { "hostage2", new RadarMarker()  },
-            { "hostage3", new RadarMarker()  },
-            { "hostage4", new RadarMarker()  },
-            { "hostage5", new RadarMarker()  }
+            { "hostage1", new RadarMarker() },
+            { "hostage2", new RadarMarker() },
+            { "hostage3", new RadarMarker() },
+            { "hostage4", new RadarMarker() },
+            { "hostage5", new RadarMarker() }
         };
 
         public struct RadarMarker 
@@ -140,11 +246,14 @@ namespace csgo_minimap_radar_helper
             public double XPoint { get; set; }
             public double YPoint { get; set; }
 
-            public RadarMarker(bool set, double x, double y)
+            public Image OnScreenImage { get; set; }
+
+            public RadarMarker(bool set, double x, double y, Image image)
             {
                 IsSet = set;
                 XPoint = x;
                 YPoint = y;
+                OnScreenImage = image;
             }
         }
     }
